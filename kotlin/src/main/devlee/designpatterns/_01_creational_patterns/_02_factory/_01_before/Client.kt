be4ef2c6d456d5cdf2faef9aa9ee9d1d9090eab1
JangHee96 <sj90947@gmail.com>
2022-05-09 21:03:43 +0900

    firts commit

---
+++ kotlin/src/main/devlee/designpatterns/_01_creational_patterns/_02_factory/_01_before/Client.kt
@@ -0,0 +1,11 @@
+package main.devlee.designpatterns._01_creational_patterns._02_factory._01_before
+
+class Client {
+    fun main() {
+        println("hello world")
+    }
+}
+
+fun main() {
+    Client().main()
+}
